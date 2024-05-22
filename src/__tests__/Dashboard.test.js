1edf18049661f8ee350e50cf79c7d2e59dbb1769
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-05-22 16:56:46 +0900

    add decralations

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,4 +1,5 @@
 import Dashboard from "@/components/pages/Dashboard.vue";
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
 import { config, createLocalVue, mount } from "@vue/test-utils";
 import Vuex from "vuex";
 jest.mock("chart.js");
