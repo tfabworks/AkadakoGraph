d3082c6bff26d60d6cd1790b5e09fed492945325
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-07-04 15:33:21 +0900

    ユーザ名のリストアが漏れていたので修正

--- src/store/modules/share.js
+++ src/store/modules/share.js
@@ -171,6 +171,8 @@ const actions = {
       localStorage.setItem(`${STORAGE_PREFIX}userID`, crypto.randomUUID())
     }
     commit('setUserID', localStorage.getItem(`${STORAGE_PREFIX}userID`))
+    // ユーザ名をlocalStorageからリストア
+    commit('setUserName', localStorage.getItem(`${STORAGE_PREFIX}userName`) ?? '')
     // チャートIDをlocalStorageからリストアor作成
     if (!/^[0-9a-f-]{32,36}/.test(localStorage.getItem(`${STORAGE_PREFIX}chartID`))) {
       localStorage.setItem(`${STORAGE_PREFIX}chartID`, crypto.randomUUID())
@@ -316,10 +318,19 @@ const actions = {
     dispatch('updateChartImage')
   },
   canUpdateChart({ rootGetters, state }) {
-    if (state.roomID === '' || state.chartID === '' || state.userID === '' || state.userName === '' || state.sharePaused) {
+    const shouldPause = rootGetters['firmata/shouldPause']
+    const { roomID, roomName, userID, userName, chartID, chartName, sharePaused } = state
+    console.log({ shouldPause, roomID, roomName, userID, userName, chartID, chartName, sharePaused })
+    if (sharePaused) {
       return false
     }
-    const shouldPause = rootGetters['firmata/shouldPause']
+    if (state.roomID === '' || state.chartID === '' || state.userID === '') {
+      return false
+    }
+    if (state.roomName === '' || state.userName === '') {
+      return false
+    }
+    console.log(shouldPause)
     if (shouldPause) {
       return false
     }
